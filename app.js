b8a91284f3c29f1d8f326626b486e4eb1f3daab2
jiangzhe01 <1107879936@qq.com>
2022-06-16 11:25:12 +0800

    安装bcrypt密码加密, joi参数校验

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const cors = require('cors')
-const morgan = require('morgan')
+const cors = require("cors");
+const morgan = require("morgan");
 
 const config = require("./config/index");
+const indexRouter = require("./routes/index");
+require("./model");
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(morgan)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
 
-app.use((req, res)=> {
-    res.send('hello world')
-})
+app.use("/api", indexRouter);
+
+app.use(require("./middleware/error"));
 
 app.listen(config.app.port, () => {
   console.log(`server is running at http://localhost:${config.app.port}`);
