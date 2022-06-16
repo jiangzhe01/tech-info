99a86543f1be78ab4dd79a353047af347c881b44
jiangzhe01 <1107879936@qq.com>
2022-06-16 11:24:25 +0800

    配置mongodb链接

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-    // 项目配置
-    app: {
-        port: process.env.PORT || 3000
-    },
-    // 数据库配置
-    db: {
-
-    }
-}
+  // 项目配置
+  app: {
+    port: process.env.PORT || 3000,
+  },
+  // 数据库配置
+  db: {
+    url: process.env.MONGODB_URL || "mongodb://localhost:27017/tech_info",
+  },
+};
